62c273373e15cb72c26a3422b2373c0a893374c2
Matthias Diener <mdiener@illinois.edu>
2023-10-19 13:20:47 -0500

    mypy

--- orderedsets/__init__.py
+++ orderedsets/__init__.py
@@ -26,7 +26,7 @@ SOFTWARE.
 try:
     import importlib.metadata as importlib_metadata
 except ModuleNotFoundError:
-    import importlib_metadata
+    import importlib_metadata  # type: ignore[no-redef]
 
 __version__ = importlib_metadata.version(__package__ or __name__)
 
