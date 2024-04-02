eebcc7c627942a220b7ee3fd38a534bdef423f8a
Matthias Diener <mdiener@illinois.edu>
2024-04-01 19:41:18 -0500

    run both cases

--- examples/speed.py
+++ examples/speed.py
@@ -12,59 +12,56 @@ from orderedset import OrderedSet as cython_ordered_set  # noqa: F401, N813
 import orderedsets
 from orderedsets import FrozenOrderedSet, OrderedSet  # noqa: F401
 
-# Case 1: 1 item
-base_set = {1}
-
-# Case 2: 1000 items
-# base_set = set(range(1000))
-
-print("base_set length:", len(base_set))
 print(f"Versions:\norderedsets={orderedsets.__version__},\n"
       f"orderedset={orderedset.__version__},\n"
       f"ordered_set={ordered_set.__version__}")
 
-for set_impl in ("set", "frozenset", "OrderedSet", "FrozenOrderedSet",
-                 "cython_ordered_set", "ordered_set2"):
 
-    print(set_impl)
+for base_set in [{1}, set(range(1000))]:
+    print("\nbase_set length:", len(base_set), "\n=====================")
 
-    print("  creation\t", timeit(f"{set_impl}({base_set})",
-                                 number=10000, globals=globals()))
+    for set_impl in ("set", "frozenset", "OrderedSet", "FrozenOrderedSet",
+                     "cython_ordered_set", "ordered_set2"):
 
-    try:
-        print("  hash\t\t", timeit(f"hash({set_impl}({base_set}))",
-                                   number=10000, globals=globals()))
-    except (AttributeError, TypeError):
-        print("  hash MISSING")
+        print(set_impl)
 
-    try:
-        print("  hash2\t\t", timeit("for i in range(1000): hash(x)",
-                                    setup=f"x={set_impl}({base_set})",
-                                    number=10000, globals=globals()))
-    except (AttributeError, TypeError):
-        print("  hash2 MISSING")
-
-    print("  elem_access\t", timeit("next(iter(x))",
-                                    setup=f"x={set_impl}({base_set})",
-                                    number=10000, globals=globals()))
-
-    print("  list(keys)\t", timeit(f"list({set_impl}({base_set}))",
-                                   number=10000, globals=globals()))
-
-    print("  union\t\t", timeit("x.union(y)",
-                                setup=f"x={set_impl}({base_set});"
-                                      f"y={set_impl}({base_set})",
-                                number=10000, globals=globals()))
-
-    print("  intersection\t", timeit("x.intersection(y)",
-                                     setup=f"x={set_impl}({base_set});"
-                                           f"y={set_impl}({base_set})",
+        print("  creation\t", timeit(f"{set_impl}({base_set})",
                                      number=10000, globals=globals()))
 
-    try:
-        print("  add\t\t", timeit("x.add(y)",
-                                  setup=f"x={set_impl}({base_set});"
-                                        f"y={next(iter(base_set))}",
-                                  number=10000, globals=globals()))
-    except AttributeError:
-        print("  add MISSING")
+        try:
+            print("  hash\t\t", timeit(f"hash({set_impl}({base_set}))",
+                                       number=10000, globals=globals()))
+        except (AttributeError, TypeError):
+            print("  hash MISSING")
+
+        try:
+            print("  hash2\t\t", timeit("for i in range(1000): hash(x)",
+                                        setup=f"x={set_impl}({base_set})",
+                                        number=10000, globals=globals()))
+        except (AttributeError, TypeError):
+            print("  hash2 MISSING")
+
+        print("  elem_access\t", timeit("next(iter(x))",
+                                        setup=f"x={set_impl}({base_set})",
+                                        number=10000, globals=globals()))
+
+        print("  list(keys)\t", timeit(f"list({set_impl}({base_set}))",
+                                       number=10000, globals=globals()))
+
+        print("  union\t\t", timeit("x.union(y)",
+                                    setup=f"x={set_impl}({base_set});"
+                                          f"y={set_impl}({base_set})",
+                                    number=10000, globals=globals()))
+
+        print("  intersection\t", timeit("x.intersection(y)",
+                                         setup=f"x={set_impl}({base_set});"
+                                               f"y={set_impl}({base_set})",
+                                         number=10000, globals=globals()))
+
+        try:
+            print("  add\t\t", timeit("x.add(y)",
+                                      setup=f"x={set_impl}({base_set});"
+                                            f"y={next(iter(base_set))}",
+                                      number=10000, globals=globals()))
+        except AttributeError:
+            print("  add MISSING")
