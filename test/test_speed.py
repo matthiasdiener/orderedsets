551786a5b254372c097b3f676808d8e49dc0dcc8
Matthias Diener <mdiener@illinois.edu>
2024-05-12 21:55:43 -0600

    bump discard speed (#56)

--- test/test_speed.py
+++ test/test_speed.py
@@ -34,11 +34,11 @@ import pytest
     [
         ("set(range(1000))", "", 3, False, False),  # init
         ("hash(s)", "s = set(range(1000))", 10, True, False),  # hash
-        ("len(s)", "s = set(range(1000))", 8, False, False),  # len
+        ("len(s)", "s = set(range(1000))", 10, False, False),  # len
         ("set(range(1000)).union(set(range(1001)))", "", 10, False, False),  # union
         ("for e in s: pass", "s = set(range(1000))", 1.2, False, False),  # iter
         ("for i in range(1000): i in s", "s = set(range(500))", 15, False, False),  # contains  # noqa: E501
-        ("for i in range(1000): s.discard(10011)", "s = set(range(500))", 6, False, True),  # discard  # noqa: E501
+        ("for i in range(1000): s.discard(10011)", "s = set(range(500))", 8, False, True),  # discard  # noqa: E501
     ],
 )
 def test_speed(statement: str, _setup: str, max_slowdown_factor: float,
